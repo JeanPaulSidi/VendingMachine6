3ff118046fcf46afacba3573c761632abd36e228
JeanPaulSidi <paulsidi@outlook.com>
2025-03-11 05:15:06 -0700

    Added a new class - Item
    Added a class Item as a blueprint to create the vending items

---
+++ VendingMachine6/Item.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine6
+{
+    public class Item
+    {
+        private string _name;
+        private decimal _price;
+        private int _count;
+        public Item(string Name, decimal Price, int Count)
+        {
+            _name = Name;
+            _price = Price;
+            _count = Count;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public decimal Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+        public int Count
+        {
+            get { return _count; }
+            set { _count = value; }
+        }
+
+    }
+}
